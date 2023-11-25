51b66d058b8bce7453dc958070c7c627c85a8e8d
Nolverto Urias Obeso <nolvertou@gmail.com>
2023-11-25 15:35:33 -0700

    Add SerializedName into data class QuoteModel to get values with a different field

--- app/src/main/java/nou/com/example/examplemvvm/data/model/QuoteModel.kt
+++ app/src/main/java/nou/com/example/examplemvvm/data/model/QuoteModel.kt
@@ -1,6 +1,11 @@
-package nou.com.example.examplemvvm.model
+package nou.com.example.examplemvvm.data.model
 
+import com.google.gson.annotations.SerializedName
+
+// TODO (12): If the fields are named different, we must use SerializedName
+// An annotation that indicates this member should be serialized to JSON with the provided
+// name value as its field name
 data class QuoteModel(
-    val quote:String,
-    val author:String
+    @SerializedName("quote")  val quote:String,
+    @SerializedName("author") val author:String
 )
