1148534d381b0bf518acc3ea67576a931840d7fd
Nolverto Urias Obeso <nolvertou@gmail.com>
2023-11-25 15:41:37 -0700

    remove hardcoded list of QuoteModel and create an empty quotes list

--- app/src/main/java/nou/com/example/examplemvvm/data/model/QuoteProvider.kt
+++ app/src/main/java/nou/com/example/examplemvvm/data/model/QuoteProvider.kt
@@ -1,30 +1,15 @@
-package nou.com.example.examplemvvm.model
+package nou.com.example.examplemvvm.data.model
 
+// TODO (15):
+//    15.1. Remove quote List and random function
+//    15.2  Create quotes list and declered as emptyList
 class QuoteProvider{
 
     // Notes:
     //      Companion Object is like a Static Object
     //      So, everyone can access into it
     companion object{
-        private val quote = listOf<QuoteModel>(
-            QuoteModel("Example 1 ", "Author 1"),
-            QuoteModel("Example 2 ", "Author 2"),
-            QuoteModel("Example 3 ", "Author 3"),
-            QuoteModel("Example 4 ", "Author 4"),
-            QuoteModel("Example 5 ", "Author 5"),
-            QuoteModel("Example 6 ", "Author 6"),
-            QuoteModel("Example 7 ", "Author 7"),
-            QuoteModel("Example 8 ", "Author 8"),
-            QuoteModel("Example 9 ", "Author 9"),
-            QuoteModel("Example 10 ", "Author 10")
-        )
+        var quotes: List<QuoteModel> = emptyList()
 
-        /**
-         * Returns a random QuoteModel
-         */
-        fun random(): QuoteModel{
-            val position = (0..9).random()
-            return quote[position]
-        }
     }
 }
