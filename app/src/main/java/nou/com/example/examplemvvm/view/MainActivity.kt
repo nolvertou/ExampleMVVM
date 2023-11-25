90ee1898f55316927456f787323a76d71c52b6fc
Nolverto Urias Obeso <nolvertou@gmail.com>
2023-11-25 11:08:51 -0700

    Creating the View

---
+++ app/src/main/java/nou/com/example/examplemvvm/view/MainActivity.kt
@@ -0,0 +1,35 @@
+package nou.com.example.examplemvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import nou.com.example.examplemvvm.databinding.ActivityMainBinding
+import nou.com.example.examplemvvm.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+    // TODO (5): Add binding and setContentView
+    private lateinit var binding: ActivityMainBinding
+
+    // TODO (9): Create a QuoteViewModel
+    // by viewModels is going to do all the connections between viewModel and activity
+    // this is achieved using the activity library already implemented
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // It observes a change in the quote and author and display it to the view
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
+
+        // Call the function randomQuote of quoteViewModel to get a randomized quote
+        binding.viewContainer.setOnClickListener{
+            quoteViewModel.randomQuote()
+        }
+    }
+}
